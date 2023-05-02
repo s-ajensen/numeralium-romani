861727babbaf363e522353f1fd61f4dfe5feab2e
Alex Jensen <alex@endure.io>
2023-05-02 15:15:03 -0400

    Correctly parses "less than"

--- spec/numeralium_romani/core_spec.clj
+++ spec/numeralium_romani/core_spec.clj
@@ -2,6 +2,14 @@
   (:require [speclj.core :refer :all]
             [numeralium-romani.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "converts roman to arabic numerals"
+  (it "parses i as 1"
+    (should= 1 (as-arabic "I")))
+
+  (it "parses consecutive i characters"
+    (should= 2 (as-arabic "II"))
+    (should= 3 (as-arabic "III")))
+
+  (it "parses iv as 4"
+    (should= 4 (as-arabic "IV"))
+    (should= 9 (as-arabic "IX"))))
--- src/numeralium_romani/core.clj
+++ src/numeralium_romani/core.clj
@@ -1,5 +1,14 @@
-(ns numeralium-romani.core)
+(ns numeralium-romani.core
+  (:require [clojure.string :as string]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(defn as-arabic [n]
+  (let [digits {\I 1 \V 5 \X 10}]
+    (->> (vec n)
+      (map #(digits %))
+      (#(loop [digits %
+               total  0]
+          (if (empty? digits)
+            total
+            (recur
+              (next digits)
+              (* (if (> (nth digits 1 0) (first digits)) -1 1) (+ total (first digits))))))))))
