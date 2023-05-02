d4c12bfb2d392d3a24da099c458668ca37f5061c
Alex Jensen <alex@endure.io>
2023-05-02 18:45:50 -0400

    Finished arabic conversion by coalescing identical consecutive digits

--- spec/numeralium_romani/core_spec.clj
+++ spec/numeralium_romani/core_spec.clj
@@ -11,8 +11,24 @@
     (should= 3 (as-arabic "III")))
 
   (it "parses iv as 4"
-    (should= 4 (as-arabic "IV"))
-    (should= 9 (as-arabic "IX")))
+    (should= 4 (as-arabic "IV")))
+
+  (it "parses leading modifiers as 'less than'"
+    (should= 9 (as-arabic "IX"))
+    (should= 90 (as-arabic "XC"))
+    (should= 900 (as-arabic "CM")))
 
   (it "parses vi as 6"
-    (should= 6 (as-arabic "VI"))))
+    (should= 6 (as-arabic "VI")))
+
+  (it "parses trailing modifiers as 'greater than'"
+    (should= 7 (as-arabic "VII"))
+    (should= 8 (as-arabic "VIII"))
+    (should= 120 (as-arabic "CXX"))
+    (should= 60 (as-arabic "LX")))
+
+  (it "parses numerals with combined leading and trailing modifiers"
+    (should= 1900 (as-arabic "MCM"))
+    (should= 2421 (as-arabic "MMCDXXI"))
+    (should= 1009 (as-arabic "MIX"))
+    (should= 3999 (as-arabic "MMMCMXCIX"))))
--- src/numeralium_romani/core.clj
+++ src/numeralium_romani/core.clj
@@ -1,11 +1,14 @@
 (ns numeralium-romani.core
   (:require [clojure.string :as string]))
 
+(def digits {\I 1 \V 5 \X 10 \L 50 \C 100 \D 500 \M 1000})
+
 (defn subtotal-coefficient [first second]
-  (if (> second first) -1 1))
+  (if (>= second first) 1 -1))
 
 (defn as-arabic [n]
-  (let [digits {\I 1 \V 5 \X 10}]
-    (->> (vec n)
-      (map #(digits %))
-      (reduce #(+ (* (subtotal-coefficient %1 %2) %1) %2)))))
+  (->> (reverse (vec n))
+    (map #(digits %))
+    (partition-by identity)
+    (map #(reduce + %))
+    (reduce #(+ (* (subtotal-coefficient %1 %2) %2) %1))))
