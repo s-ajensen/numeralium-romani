d5e4f6fe0301b3cd30c4bdca97489189c58ecf3e
Alex Jensen <alex@endure.io>
2023-05-05 12:42:24 -0400

    Doesn't work

--- spec/numeralium_romani/core_spec.clj
+++ spec/numeralium_romani/core_spec.clj
@@ -2,6 +2,60 @@
   (:require [speclj.core :refer :all]
             [numeralium-romani.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "converts arabic to roman numerals"
+  (it "parses single character numbers up to 1,000"
+    (should= "I" (as-roman 1))
+    (should= "V" (as-roman 5))
+    (should= "X" (as-roman 10))
+    (should= "L" (as-roman 50))
+    (should= "C" (as-roman 100))
+    (should= "M" (as-roman 1000)))
+
+  (it "parses subtraction-based numerals up to 900"
+    (should= "IV" (as-roman 4))
+    (should= "IX" (as-roman 9))
+    (should= "CD" (as-roman 400))
+    (should= "XC" (as-roman 90))
+    (should= "CD" (as-roman 400))
+    (should= "CM" (as-roman 900)))
+
+  (it "parses identical, repeating digits"
+    (should= "II" (as-roman 2))
+    (should= "III" (as-roman 3))
+    (should= "XX" (as-roman 20))
+    (should= "CCC" (as-roman 300)))
+
+  (it "parses mixed repeating and subtraction-based numerals"
+    (should= "XXIX" (as-roman 29))
+    (should= "XIV" (as-roman 14))
+    (should= "CXCIX" (as-roman 199)))
+
+  (it "parses addition-based numerals up to 999"
+    (should= "VI" (as-roman 6))
+    (should= "LX" (as-roman 60))
+    (should= "DC" (as-roman 600))
+    (should= "CMXCIX" (as-roman 999)))
+
+  (it "parses mixed repeating and addition-based numerals"
+    (should= "LXXX" (as-roman 80))
+    (should= "DCC" (as-roman 700)))
+
+  (it "parses mixed addition- and subtraction-based numerals"
+    (should= "MCM" (as-roman 1900))
+    (should= "MIX" (as-roman 1009))
+    (should= "MLXVI" (as-roman 1066))
+    (should= "MMMCMXCIX" (as-roman 3999))))
+
+(describe "converts roman to arabic numerals"
+  (it "parses single character numerals"
+    (should= 1 (as-arabic "I"))
+    (should= 5 (as-arabic "V"))
+    (should= 1000 (as-arabic "M")))
+
+  (it "parses subtraction-based numerals"
+    (should= 4 (as-arabic "IV"))
+    (should= 90 (as-arabic "XC"))
+    (should= 900 (as-arabic "CM")))
+
+  (it "parses repeated single character numerals"
+    (should= 3 (as-arabic "III"))))
--- src/numeralium_romani/core.clj
+++ src/numeralium_romani/core.clj
@@ -1,5 +1,23 @@
-(ns numeralium-romani.core)
+(ns numeralium-romani.core
+  (:require [clojure.set :as set]
+            [clojure.string :as string]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(def arabic-to-roman {1 "I" 4 "IV" 5 "V" 9 "IX" 10 "X" 50 "L" 90 "XC" 100 "C" 400 "CD" 500 "D" 900 "CM" 1000 "M"})
+(def roman-to-arabic (sort #(compare (count %1) (count %2)) (set/map-invert arabic-to-roman)))
+
+(defn as-roman [n]
+  (loop [n n roman ""]
+    (if (zero? n)
+      roman
+      (let [max-digit (last (sort (filter #(>= n (key %)) arabic-to-roman)))]
+        (recur (- n (key max-digit)) (str roman (val max-digit)))))))
+
+(defn as-arabic [n]
+  (loop [n (string/split n #"") arabic 0]
+    (if (empty? n)
+      arabic
+      (recur (drop 1 n) (+ arabic (roman-to-arabic (first n)))))))
+
+
+(defn as-arabic [n]
+  (let []))
