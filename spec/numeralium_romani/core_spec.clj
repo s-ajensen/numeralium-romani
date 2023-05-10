8e655e4cac2080cc4f48331923f1f6c8740b61c6
Alex Jensen <alex@endure.io>
2023-05-10 16:04:35 -0400

    Works

--- spec/numeralium_romani/core_spec.clj
+++ spec/numeralium_romani/core_spec.clj
@@ -2,6 +2,27 @@
   (:require [speclj.core :refer :all]
             [numeralium-romani.core :refer :all]))
 
-(describe "a test"
-  (it "FIXME, I fail."
-    (should= 0 1)))
+(describe "convert roman to arabic numerals"
+  (it "parses < 3 as tallies"
+    (should= 1 (as-arabic "I"))
+    (should= 2 (as-arabic "II"))
+    (should= 3 (as-arabic "III")))
+
+  (it "parses subtraction-based numerals"
+    (should= 4 (as-arabic "IV"))
+    (should= 9 (as-arabic "IX")))
+
+  (it "parses single char numerals"
+    (should= 5 (as-arabic "V"))
+    (should= 10 (as-arabic "X"))
+    (should= 50 (as-arabic "L"))
+    (should= 100 (as-arabic "C")))
+
+  (it "parses addition-based numerals"
+    (should= 6 (as-arabic "VI"))
+    (should= 11 (as-arabic "XI")))
+
+  (it "parses combined addition- and subtraction- based numerals"
+    (should= 14 (as-arabic "XIV"))
+    (should= 24 (as-arabic "XXIV"))
+    (should= 1900 (as-arabic "MCM"))))
--- src/numeralium_romani/core.clj
+++ src/numeralium_romani/core.clj
@@ -1,5 +1,15 @@
-(ns numeralium-romani.core)
+(ns numeralium-romani.core
+  (:require [clojure.string :as str]))
 
-(defn -main
-  [& args]
-  (println "Hello World"))
+(def digits { "I" 1 "V" 5 "X" 10 "L" 50 "C" 100 "M" 1000 })
+
+(defn subtotal-coefficient [first second]
+  (if (< second first) -1 1))
+
+(defn as-arabic [n]
+  (->> (str/split n #"")
+    (reverse)
+    (map #(digits %))
+    (partition-by identity)
+    (map #(reduce + %))
+    (reduce #(+ (* (subtotal-coefficient %1 %2) %2) %1))))
